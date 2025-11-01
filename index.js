6940d5073ffa7e76b7914660f4b376fc79d40b69
siamxlr3 <siamxlr3@gmail.com>
2025-11-02 02:19:25 +0600

    f13

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: [
-        '*'
-    ],
+    origin: ['https://aurellia-e-commerce-indol.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 

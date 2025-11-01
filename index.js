943e64663741b444e551ea4643e623adf3264401
siamxlr3 <siamxlr3@gmail.com>
2025-11-02 02:38:33 +0600

    g1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.options('*', cors({
+app.use(cors({
     origin: ['https://aurellia.vercel.app'],
     credentials: true,
 }));
